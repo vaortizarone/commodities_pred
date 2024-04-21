6e4cb565ca9757e907dcd576e1071016b2dc542b
alejandro <121509311+vaortizarone@users.noreply.github.com>
2024-04-21 00:41:05 -0500

    bcrp data ok

--- scrap_bcrp.py
+++ scrap_bcrp.py
@@ -1,1 +1,9 @@
-print("ya")
+import requests
+
+url = "https://estadisticas.bcrp.gob.pe/estadisticas/series/api/PD31889XD-PD31888XD-PD31887XD-PD04705XD-PD04702XD-PD04704XD-PD04703XD-PD04701XD/json/2000-01-03/2024-04-20"
+
+response = requests.get(url)
+
+data = response.json()
+
+data
